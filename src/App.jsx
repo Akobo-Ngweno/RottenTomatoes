2088564d33f90f17e5170adb870fcc4bb184eb9d
Akobo-Ngweno <ngwenojacob@gmail.com>
2025-10-03 09:55:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MenuBar from "./components/MenuBar"
+
+function App() {
+  
+
+  return (
+    <>
+      <MenuBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+export default function MenuBar(){
+        return(
+            <>
+            <button>search</button>
+            <nav>
+                <div>
+                    <ul>
+                        <li>What's the Tomatometer {'\u00AE'} ?</li>
+                        <li>Critics</li>
+                        <li>LOGIN /SIGNUP</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                    <li>Movies</li>
+                    <li>Tv Shows</li>
+                    <li>Movie Trivia</li>
+                    <li>News</li>
+                    <li>ShowTimes</li>
+                </ul>
+                </div>
+                
+            </nav>
+            
+            </>
+        )
+}
